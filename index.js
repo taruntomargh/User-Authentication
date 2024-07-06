7ffed1ede8cc9368a25575b3c21458e994b1cb6e
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-07-06 12:52:15 +0530

    simple hello world server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res)=>{
+    res.send("Hello world");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at http://localhost:${PORT}`);    
+});
