75e33385155b7e67b43f7441020d93d77a438c01
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-07-07 15:01:24 +0530

    Add user schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
