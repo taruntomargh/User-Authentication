3086c8f60c8815e046b7b8cd17721abb8797309a
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-07-07 15:08:25 +0530

    Set up db connection, routes, ejs, url-encoded middleware parser

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const {connectDB} = require("./db/connect");
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req, res)=>{
-    res.send("Hello World !!!")
+// MongoDB connection
+connectDB("mongodb://localhost:27017/userAuth").then(()=>{
+    console.log("MongoDB connected");
 });
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Route
+app.use('/', userRoute);
+
+// Template Engines
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening at http://localhost:8000`);
