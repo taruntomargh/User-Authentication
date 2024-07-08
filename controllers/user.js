5475a899b9fd9d090941fcee7d4607061c32addd
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-07-08 12:47:04 +0530

    Add password hash for signup and compare for signin

--- controllers/user.js
+++ controllers/user.js
@@ -1,32 +1,46 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 async function handleUserSignup(req, res) {
-  const { fullname, email, password } = req.body;
-  console.log(fullname, email, password);
+  try {
+    const { fullname, email, password } = req.body;
 
-  const user = await User.create({
-    fullname: fullname,
-    email: email,
-    password: password,
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      if(err){
+        console.log("Password not hashed");
+      }
+        await User.create({
+        fullname: fullname,
+        email: email,
+        password: hash,
+      });
+    })
   });
 
-  if (!user) {
-    return res.render("/signup", { error: "User not created. Try Again" });
-  }
-
   return res.redirect("/signin");
+  
+  } catch (err) {
+    console.log("ERROR: User is not signed up.");
+  }
 }
 
 async function handleUserSignin(req, res) {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email: email, password: password });
+  const user = await User.findOne({ email: email });
 
-  if (!user) {
-    return res.render("/signin", { error: "User not found. Try Again" });
-  }
-
-  return res.redirect("/");
+  bcrypt.compare(password, user.password, function (err, result) {
+    try {
+      if(result) {
+        return res.redirect("/");
+      } else {
+        return res.render("/signin", { error: "incorrect Username/Password." });
+      }
+    } catch (err) {
+      console.log("ERROR: Something went wrong. handleUserSignin");
+    }
+  });
 }
 
 module.exports = {
